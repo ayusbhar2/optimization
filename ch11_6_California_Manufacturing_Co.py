78aa61637c177de0eb8663686bcb1a35709d350d
Ayush Bharadwaj <4387161+ayusbhar2@users.noreply.github.com>
2023-05-13 20:55:02 -0700

    tab to spaces

--- ch11_6_California_Manufacturing_Co.py
+++ ch11_6_California_Manufacturing_Co.py
@@ -13,9 +13,9 @@ x4 = cp.Variable(1, boolean=True, name='x4')
 obj = cp.Maximize(9 * x1 + 5 * x2 + 6 * x3 + 4 * x4)
 
 constraints = [6 * x1 + 3 * x2 + 5 * x3 + 2 * x4 <= 10,
-			   						 x3 + 	  x4 <= 1,
-			   	  -x1  		   + x3 		 	 <= 0,
-			   	  		   -x2 				+ x4 <= 0]
+                                     x3 +     x4 <= 1,
+                  -x1          + x3              <= 0,
+                           -x2              + x4 <= 0]
 
 bip = cp.Problem(obj, constraints)
 
@@ -29,7 +29,7 @@ print('solving lp1...')
 print('status: {}'.format(lp1.status))
 print('optimal value: {}'.format(lp1.value))
 print('optimal solution: {}'.format(
-	[v.value[0]for v in lp1.variables()]))
+    [v.value[0]for v in lp1.variables()]))
 
 # Subprob 2 (x1 = 1)
 sub2 = cp.Problem(obj, constraints + [x1 >= 1])
@@ -39,4 +39,4 @@ print('solving lp2...')
 print('status: {}'.format(lp2.status))
 print('optimal value: {}'.format(lp2.value))
 print('optimal solution: {}'.format(
-	[v.value[0]for v in lp2.variables()]))
+    [v.value[0]for v in lp2.variables()]))
