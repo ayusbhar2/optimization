284784076f3117b36aa8c0b42f7cb91305079f1f
Ayush Bharadwaj <4387161+ayusbhar2@users.noreply.github.com>
2023-05-13 20:56:37 -0700

    more tabs to spaces

--- utils.py
+++ utils.py
@@ -3,10 +3,10 @@ import cvxpy as cp
 import numpy as np
 
 def get_lp_relaxation_of_bip(bip):
-	new_constraints = []
-	for v in bip.variables():
-		if v.attributes['boolean']:
-			v.attributes['boolean'] = False
-			new_constraints += [0 <= v, v <= 1]
-	lp = cp.Problem(bip.objective, bip.constraints + new_constraints)
-	return lp
+    new_constraints = []
+    for v in bip.variables():
+        if v.attributes['boolean']:
+            v.attributes['boolean'] = False
+            new_constraints += [0 <= v, v <= 1]
+    lp = cp.Problem(bip.objective, bip.constraints + new_constraints)
+    return lp
