0c7aec5ec07cd8268c25d5a531e76d3b4e755db2
Ayush Bharadwaj <4387161+ayusbhar2@users.noreply.github.com>
2023-05-15 18:19:32 -0700

    modl with demand profile

--- solver/utils.py
+++ solver/utils.py
@@ -4,3 +4,16 @@ import numpy as np
 
 def is_integer_solution(solution: list, epsilon: float):
     return all([abs(x - int(x)) <= epsilon for x in solution])
+
+def get_result_summary(prob: cp.problems.problem.Problem):
+    result = {'status': None,
+              'optimal_value': None,
+              'optimal_solution': None}
+
+    result['status'] = prob.status
+    result['optimal_value'] = prob.value
+    if prob.status == 'optimal':
+        result['optimal_solution'] = dict(
+        [(v.name(), v.value[0]) for v in prob.variables()])
+
+    return result
